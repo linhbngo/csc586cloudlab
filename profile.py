a5683e610599682aafbe44965580e4c863d0cfed
Linh B Ngo <lngo@wcupa.edu>
2019-07-22 17:20:15 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -17,6 +17,8 @@ who is to be authenticated using LDAP.
 tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 request.addTour(tour)
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
 
 for i in range(2):
   if i == 0:
@@ -26,6 +28,10 @@ for i in range(2):
    
   node.routable_control_ip = "true"  
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
   
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
