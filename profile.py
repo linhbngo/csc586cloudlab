4f6e6be4886f41bbb7a7e8bd38609754f079521a
Linh B Ngo <lngo@wcupa.edu>
2019-07-28 20:58:19 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -7,8 +7,8 @@ request = pc.makeRequestRSpec()
 
 tourDescription = \
 """
-This profile provides a two-node set to study SSO. One node will be the LDAP server, and the other node is a client
-who is to be authenticated using LDAP. 
+This profile provides a three-node set to study NFS/SSO. One node will be the LDAP server, one node will be the NFS server, 
+and the remaining node is an NFS client. Both NFS nodes will be authenticated using LDAP. 
 """
 
 #
@@ -20,12 +20,13 @@ request.addTour(tour)
 prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
-for i in range(2):
+for i in range(3):
   if i == 0:
     node = request.XenVM("ldapserver")    
+  elif i == 1:
+    node = request.XenVM("nfsserver")
   else:
-    node = request.XenVM("ldapclient")
-   
+    node = request.XenVM("nfsclient")
   node.routable_control_ip = "true"  
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
   iface = node.addInterface("if" + str(i))
